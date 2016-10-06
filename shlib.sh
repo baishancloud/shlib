733fa5233bccd5af680ff3fd0e2c45c3d0f2b155
drdr xp <drdr.xp@gmail.com>
2016-10-06 13:09:24 +0800

    add shlib_git.sh

---
+++ shlib.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+source shlib_color.sh
+source shlib_log.sh
+source shlib_git.sh
---
+++ shlib_git.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+source shlib/shlib_log.sh
+
+git_hash()
+{
+    git rev-parse $1 \
+        || die "'git_hash $@'"
+}
+git_rev_list()
+{
+    # --parents
+    # print parent in this form:
+    #     <commit> <parent-1> <parent-2> ..
+
+    git rev-list \
+        --reverse \
+        --topo-order \
+        --default HEAD \
+        --simplify-merges \
+        "$@" \
+        || die "'git rev-list $@'"
+}
+git_ver()
+{
+    local git_version=$(git --version | awk '{print $NF}')
+    local git_version_1=${git_version%%.*}
+    local git_version_2=${git_version#*.}
+    git_version_2=${git_version_2%%.*}
+
+    printf "%03d%03d000" $git_version_1 $git_version_2
+}
+git_working_root()
+{
+    git rev-parse --show-toplevel
+}
+
+git_branch_default_remote()
+{
+    local branchname=$1
+    git config --get branch.${branchname}.remote
+}
+git_branch_default_upstream_ref()
+{
+    local branchname=$1
+    git config --get branch.${branchname}.merge
+}
+git_branch_default_upstream()
+{
+    git_branch_default_upstream_ref "$@" | sed 's/^refs\/heads\///'
+}
+
+git_head_branch()
+{
+    git symbolic-ref --short HEAD
+}
