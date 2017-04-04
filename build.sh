20ccbecd81500b527339f662c0a7ef1f1e6678df
drdr xp <drdr.xp@gmail.com>
2017-04-04 23:22:30 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python2 embed.py shlib.sh        dist
+python2 embed.py shlib_color.sh  dist
+python2 embed.py shlib_format.sh dist
+python2 embed.py shlib_git.sh    dist
+python2 embed.py shlib_log.sh    dist
